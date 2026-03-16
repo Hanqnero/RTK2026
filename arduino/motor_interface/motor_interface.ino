d7d5e7dcc6005fca903cfcd6dbdb4cf1920a220a
Alexey <nethergolem3@gmail.com>
2026-03-16 18:35:35 +0300

    use struct for rx

--- arduino/motor_interface/motor_interface.ino
+++ arduino/motor_interface/motor_interface.ino
@@ -40,11 +40,14 @@ inline void right_stop() {
 // right_backward_pwm [1]
 byte rx_buf[4];
 
-// left_enc_speed  [4]
-// left_enc_cnt    [4]
-// right_enc_speed [4]
-// right_enc_cnt   [4]
-byte tx_buf[32];
+struct __attribute__((packed)) TxPacket {
+    int32_t left_speed;   // [4] encoder speed left
+    int32_t left_cnt;     // [4] encoder count left
+    int32_t right_speed;  // [4] encoder speed right
+    int32_t right_cnt;    // [4] encoder count right
+};
+
+TxPacket tx_pkt;
 
 uint32_t last_millis;
 
@@ -73,28 +76,19 @@ void setup() {
 
 
 void loop() {
-    // Serial.write(tx_buf, 32);
-    if (Serial.available() > 0) {
-        String line = Serial.readStringUntil('\n');
-
-        int left_forward = 0;
-        int left_backward = 0;
-        int right_forward = 0;
-        int right_backward = 0;
-
-        if (sscanf(line.c_str(), "%d %d %d %d", &left_forward, &left_backward, &right_forward, &right_backward) == 4) {
-            left_forward = constrain(left_forward, 0, 255);
-            left_backward = constrain(left_backward, 0, 255);
-            right_forward = constrain(right_forward, 0, 255);
-            right_backward = constrain(right_backward, 0, 255);
-
-            int left_pwm = left_forward - left_backward;
-            int right_pwm = right_forward - right_backward;
-
-            left_set_speed(left_pwm);
-            right_set_speed(right_pwm);
-        }
+    if (Serial.available() >= 4) {
+        Serial.readBytes(rx_buf, 4);
+        int left_pwm  = (int)rx_buf[0] - (int)rx_buf[1];
+        int right_pwm = (int)rx_buf[2] - (int)rx_buf[3];
+        left_set_speed(left_pwm);
+        right_set_speed(right_pwm);
     }
+
+    tx_pkt.left_speed  = (int32_t)left_enc.speed();
+    tx_pkt.left_cnt    = (int32_t)left_enc.cnt();
+    tx_pkt.right_speed = (int32_t)right_enc.speed();
+    tx_pkt.right_cnt   = (int32_t)right_enc.cnt();
+    Serial.write((byte*)&tx_pkt, sizeof(tx_pkt));
 
     while (millis() - last_millis < 100) {
     } // Ждёт чтобы каждый цикл занимал одинаковое (время как на ВПД)
