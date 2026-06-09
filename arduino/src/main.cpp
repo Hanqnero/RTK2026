df408883e1fb25040f45cda6eb612e5d4169acc3
Alexey <nethergolem3@gmail.com>
2026-06-09 17:05:22 +0300

    Switch motor driver to dual PWM pins

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -16,8 +16,8 @@ constexpr float kPi = 3.14159265358979323846F;
 constexpr float kWheelCircumferenceM = 2.0F * kPi * kWheelRadiusM;
 constexpr float kControlPeriodS = static_cast<float>(kControlPeriodMs) / 1000.0f;
 
-GyverMotor2<GM2::DIR_DIR_PWM> left_motor(LEFT_AI1, LEFT_AI2, LEFT_PWMA);
-GyverMotor2<GM2::DIR_DIR_PWM> right_motor(RIGHT_BI1, RIGHT_BI2, RIGHT_PWMB);
+GyverMotor2<GM2::PWM_PWM_SPEED> left_motor(LEFT_PWM_A, LEFT_PWM_B);
+GyverMotor2<GM2::PWM_PWM_SPEED> right_motor(RIGHT_PWM_A, RIGHT_PWM_B);
 
 EncoderCounter left_encoder(LEFT_ENC_CLK, LEFT_ENC_DT, kLeftEncoderReverse, INPUT_PULLUP);
 EncoderCounter right_encoder(RIGHT_ENC_CLK, RIGHT_ENC_DT, kRightEncoderReverse, INPUT_PULLUP);
